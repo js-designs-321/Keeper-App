0d3f3086899629f7858642e5bec8dd1c5f7ed7b9
js-designs-321 <jatni.321@gmail.com>
2022-06-19 22:42:30 +0530

    Add arrow function

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,21 +4,18 @@ import Note from "./Note";
 import Footer from "./Footer";
 import notes from "./notes";
 
-function notesEntry(note){
-    return (
-        <Note 
-            key = {note.key}
-            title = {note.title}
-            content = {note.content}
-        />
-    );
-}
-
-function App(){
+function App() {
     return (
         <div>
             <Header />
-            {notes.map(notesEntry)}
+            {notes.map((note) => (
+                    <Note
+                        key={note.key}
+                        title={note.title}
+                        content={note.content}
+                    />
+                )
+            )}
             <Footer />
         </div>
     );
